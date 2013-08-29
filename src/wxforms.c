fd4804afaa889d1362a6f389be946dcc517ebc1a
Gregory Hinton Nietsky <gregory@distrotech.co.za>
2013-08-29 08:10:13 +0200

    Add new system wizard

--- src/wxforms.c
+++ src/wxforms.c
@@ -43,8 +43,10 @@ struct xml_doc *loadxmlconf(struct dtsgui *dtsgui) {
 	struct curlbuf *cbuf;
 	struct xml_doc *xmldoc = NULL;
 
-	auth = curl_newauth("admin", "");
-	if (!(cbuf = curl_geturl("https://ns2.distrotech.co.za:666/ns/config/netsentry.xml", auth, dtsgui_pwdialog, dtsgui))) {
+	auth = dtsgui_pwdialog("admin", "", dtsgui);
+	objunref(auth);
+
+	if (!(cbuf = curl_geturl("https://callshop.distrotech.co.za:666/cshop", auth, dtsgui_pwdialog, dtsgui))) {
 		objunref(auth);
 		return NULL;
 	}
@@ -60,16 +62,201 @@ struct xml_doc *loadxmlconf(struct dtsgui *dtsgui) {
 	return xmldoc;
 }
 
-void handle_test(dtsgui_pane p, int event, void *data) {
+int newsys_wizard(struct dtsgui *dtsgui, void *data) {
+	dtsgui_pane dp[12], pg;
+	struct dtsgui_wizard *twiz;
+	struct form_item *ilist;
+	struct xml_doc *xmldoc = NULL;
+
+	twiz = dtsgui_newwizard(dtsgui, "New System Wizard");
+
+	dp[0] = dtsgui_wizard_addpage(twiz, "Customer Information", NULL, xmldoc);
+	dp[1] = dtsgui_wizard_addpage(twiz, "Network/IP Information", NULL, xmldoc);
+	dp[2] = dtsgui_wizard_addpage(twiz, "Dynamic DNS", NULL, xmldoc);
+	dp[3] = dtsgui_wizard_addpage(twiz, "DNS", NULL, xmldoc);
+	dp[4] = dtsgui_wizard_addpage(twiz, "X.509", NULL, xmldoc);
+	dp[5] = dtsgui_wizard_addpage(twiz, "Fileserver", NULL, xmldoc);
+	dp[6] = dtsgui_wizard_addpage(twiz, "Firewall", NULL, xmldoc);
+	dp[7] = dtsgui_wizard_addpage(twiz, "Extensions", NULL, xmldoc);
+	dp[8] = dtsgui_wizard_addpage(twiz, "PBX Setup", NULL, xmldoc);
+	dp[9] = dtsgui_wizard_addpage(twiz, "Attendant", NULL, xmldoc);
+	dp[10] = dtsgui_wizard_addpage(twiz, "Truk Setup", NULL, xmldoc);
+	dp[11] = dtsgui_wizard_addpage(twiz, "Least Cost Routing", NULL, xmldoc);
+
+	pg=dp[0];
+	dtsgui_xmltextbox(pg, "Customer Name", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Contact Person", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Email Address", NULL, NULL);
+	dtsgui_xmltextbox_multi(pg, "Customer Address (Postal)", NULL, NULL);
+	dtsgui_xmltextbox_multi(pg, "Customer Address (Physical)", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Customer Tel", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Customer Company ID", NULL, NULL);
+	dtsgui_xmltextbox(pg, "VAT No.", NULL, NULL);
+
+	pg=dp[1];
+	dtsgui_xmltextbox(pg, "IPv4 Address [IP/SN]", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Domain Name", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Default Gateway", NULL, NULL);
+	dtsgui_xmltextbox(pg, "SMTP Gateway", NULL, NULL);
+	dtsgui_xmltextbox(pg, "NTP Server", NULL, NULL);
+
+	pg=dp[2];
+	dtsgui_xmltextbox(pg, "Dynamic DNS Server", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Dynamic DNS Zone", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Dynamic DNS KEY", NULL, NULL);
+
+	pg=dp[3];
+	dtsgui_xmltextbox(pg, "Primary DNS", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Secondary DNS", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Primary WINS", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Secondary WINS", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Primary MX", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Secondary MX", NULL, NULL);
+
+	pg=dp[4];
+	dtsgui_xmltextbox(pg, "Country Code", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Province/State", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Company", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Division", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Name", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Email", NULL, NULL);
+
+	pg=dp[5];
+	dtsgui_xmltextbox(pg, "Workgroup/Domain", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Aliases", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Domain Controllers", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Realm [If Joining ADS]", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Domain Controller", NULL, NULL);
+
+	pg=dp[6];
+	ilist = dtsgui_xmlcombobox(pg, "External Interface", NULL, NULL);
+	dtsgui_listbox_add(ilist, "br0", NULL);
+	dtsgui_listbox_add(ilist, "ethB", NULL);
+	dtsgui_listbox_add(ilist, "br0.100", NULL);
+	dtsgui_listbox_add(ilist, "3G", NULL);
+	objunref(ilist);
+	dtsgui_xmlcheckbox(pg, "External Device Is PPPoE", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Number/Service/APN", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Username", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Password", NULL, NULL);
+	dtsgui_xmltextbox(pg, "MTU", NULL, NULL);
+
+	pg=dp[7];
+	ilist = dtsgui_xmllistbox(pg, "Default Extension Permision", NULL, NULL);
+	dtsgui_listbox_add(ilist, "Internal Extensions", NULL);
+	dtsgui_listbox_add(ilist, "Local PSTN", NULL);
+	dtsgui_listbox_add(ilist, "Long Distance PSTN", NULL);
+	dtsgui_listbox_add(ilist, "Cellular", NULL);
+	dtsgui_listbox_add(ilist, "Premium", NULL);
+	dtsgui_listbox_add(ilist, "International", NULL);
+	objunref(ilist);
+	ilist = dtsgui_xmllistbox(pg, "Default Auth Extension Permision", NULL, NULL);
+	dtsgui_listbox_add(ilist, "Internal Extensions", NULL);
+	dtsgui_listbox_add(ilist, "Local PSTN", NULL);
+	dtsgui_listbox_add(ilist, "Long Distance PSTN", NULL);
+	dtsgui_listbox_add(ilist, "Cellular", NULL);
+	dtsgui_listbox_add(ilist, "Premium", NULL);
+	dtsgui_listbox_add(ilist, "International", NULL);
+	objunref(ilist);
+	ilist = dtsgui_xmllistbox(pg, "Default After Hours Extension Permision", NULL, NULL);
+	dtsgui_listbox_add(ilist, "Internal Extensions", NULL);
+	dtsgui_listbox_add(ilist, "Local PSTN", NULL);
+	dtsgui_listbox_add(ilist, "Long Distance PSTN", NULL);
+	dtsgui_listbox_add(ilist, "Cellular", NULL);
+	dtsgui_listbox_add(ilist, "Premium", NULL);
+	dtsgui_listbox_add(ilist, "International", NULL);
+	objunref(ilist);
+	ilist = dtsgui_xmllistbox(pg, "Snom Network Port Config", NULL, NULL);
+	dtsgui_listbox_add(ilist, "100Mb/s Full Duplex", NULL);
+	dtsgui_listbox_add(ilist, "100Mb/s Half Duplex", NULL);
+	dtsgui_listbox_add(ilist, "10Mb/s Full Duplex", NULL);
+	dtsgui_listbox_add(ilist, "10Mb/s Half Duplex", NULL);
+	dtsgui_listbox_add(ilist, "Auto", NULL);
+	objunref(ilist);
+	dtsgui_xmltextbox(pg, "Voip Vlan Interface", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Default Extension Prefix", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Auto Config Start", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Auto Config End", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Default Ring Timeout", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Require Authorization", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Lock Snom Phone Settings", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Enable Voice Mail", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Enable Call Logging", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Hangup Calls To Unknown Numbers/DDI", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Require Extension Number With PIN", NULL, NULL);
+
+	pg=dp[8];
+	dtsgui_xmltextbox(pg, "Local Area Code", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Local Extension Prefix", NULL, NULL);
+	ilist = dtsgui_xmllistbox(pg, "ISDN PRI Framing [T1-E1]", NULL, NULL);
+	dtsgui_listbox_add(ilist, "ccs - esf", NULL);
+	dtsgui_listbox_add(ilist, "cas - d4/Superframe", NULL);
+	objunref(ilist);
+	ilist = dtsgui_xmllistbox(pg, "ISDN PRI Coding [T1-E1]", NULL, NULL);
+	dtsgui_listbox_add(ilist, "hdb3 - b8zs", NULL);
+	dtsgui_listbox_add(ilist, "ami", NULL);
+	objunref(ilist);
+	dtsgui_xmlcheckbox(pg, "Calls To Internal Extensions Follow Forward Rules", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Hangup Calls To Unknown Numbers/DDI", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "ISDN BRI Immeadiate Routeing (No MSN/DDI)", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "ISDN BRI Use Round Robin Routing", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "ISDN BRI Allow Automatic Setting Of CLI (DDI Required)", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "ISDN PRI CRC4 Checking (E1 Only)", NULL, NULL);
+
+	/*XXXX PRI CONFIG LBO*/
+	pg=dp[9];
+	dtsgui_xmltextbox(pg, "Queue Timeout Checked Every 18s", NULL, NULL);
+	dtsgui_xmltextbox(pg, "Auto Attendant Mailbox/Forward On No Agent/Timeout", NULL, NULL);
+	dtsgui_xmltextbox(pg, "IVR Delay Between Digits", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Disable Default Auto Attendant Prompts", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Music On Hold When Calling Reception", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Record Inbound Calls", NULL, NULL);
+
+	pg=dp[10];
+	ilist = dtsgui_xmllistbox(pg, "Primary Trunk", NULL, NULL);
+	dtsgui_listbox_add(ilist, "None", NULL);
+	dtsgui_listbox_add(ilist, "ISDN PRI", NULL);
+	dtsgui_listbox_add(ilist, "ISDN BRI", NULL);
+	dtsgui_listbox_add(ilist, "Analogue", NULL);
+	objunref(ilist);
+	dtsgui_textbox(pg, "No. Of Trunk Ports", NULL, NULL);
+	ilist = dtsgui_xmllistbox(pg, "Gateway Trunk", NULL, NULL);
+	dtsgui_listbox_add(ilist, "None", NULL);
+	dtsgui_listbox_add(ilist, "ISDN PRI", NULL);
+	dtsgui_listbox_add(ilist, "ISDN BRI", NULL);
+	dtsgui_listbox_add(ilist, "Analogue", NULL);
+	objunref(ilist);
+	dtsgui_xmltextbox(pg, "No. Of Gateway Ports", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Extensions Are On Gateway By Default", NULL, NULL);
+
+	pg=dp[11];
+	ilist = dtsgui_xmllistbox(pg, "Protocol", NULL, NULL);
+	dtsgui_listbox_add(ilist, "SIP", NULL);
+	dtsgui_listbox_add(ilist, "IAX2", NULL);
+	objunref(ilist);
+	dtsgui_xmlcheckbox(pg, "Use DTMF INFO (SIP)", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Use SRTP (SIP)", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Use From User (SIP [Disables Sending CLI])", NULL, NULL);
+	dtsgui_xmlcheckbox(pg, "Disable Video", NULL, NULL);
+
+	dtsgui_runwizard(twiz);
+
+	objunref(twiz);
+	return 0;
+}
+
+
+void handle_test(dtsgui_pane p, int type, int event, void *data) {
 	switch(event) {
 		case wx_PANEL_BUTTON_YES:
 			printf("Ok\n");
 			break;
+
 		case wx_PANEL_BUTTON_NO:
 			printf("Cancel\n");
 			break;
-		default
-				:
+
+		default:
 			printf("Other %i\n", event);
 			break;
 	}
@@ -77,28 +264,44 @@ void handle_test(dtsgui_pane p, int event, void *data) {
 
 void testpanel(struct dtsgui *dtsgui, dtsgui_menu menu) {
 	dtsgui_pane p;
-	dtsgui_listbox l;
+	struct form_item *l;
 
-	p = dtsgui_newpanel(dtsgui, "Test Panel", wx_PANEL_BUTTON_ACTION, 1);
+	p = dtsgui_panel(dtsgui, "Test Panel", wx_PANEL_BUTTON_ALL, 1, NULL);
+	dtsgui_setevcallback(p, handle_test, NULL);
 
-	dtsgui_newtextbox(p, "TextBox", "Text");
-	dtsgui_newtextbox_multi(p, "Multi Line", "L\nL\n");
-	dtsgui_newcheckbox(p, "Checkbox", 1);
+	dtsgui_textbox(p, "TextBox", "Text", NULL);
+	dtsgui_textbox_multi(p, "Multi Line", "L\nL\n", NULL);
+	dtsgui_checkbox(p, "Checkbox", 1, NULL);
 
-	l = dtsgui_newlistbox(p, "ListBox");
+	l = dtsgui_combobox(p, "ComboBox", NULL);
 	dtsgui_listbox_add(l, "Test1", NULL);
 	dtsgui_listbox_add(l, "Test2", NULL);
 	dtsgui_listbox_add(l, "Test3", NULL);
+	dtsgui_listbox_add(l, "Alt Test3", NULL);
+	objunref(l);
+
+	l = dtsgui_listbox(p, "ListBox", NULL);
+	dtsgui_listbox_add(l, "Test1", NULL);
+	dtsgui_listbox_add(l, "Test2", NULL);
+	dtsgui_listbox_add(l, "Test3", NULL);
+	dtsgui_listbox_add(l, "Alt Test3", NULL);
+	objunref(l);
 
 	dtsgui_newmenuitem(menu, dtsgui, "&Test", p);
-	dtsgui_setevcallback(p, handle_test, NULL);
 }
 
 void file_menu(struct dtsgui *dtsgui) {
 	dtsgui_menu file;
+	dtsgui_treeview tree;
 
 	file = dtsgui_newmenu(dtsgui, "&File");
+
+	dtsgui_newmenucb(file, dtsgui, "&New System", "New System Configuration Wizard", newsys_wizard, NULL);
+	dtsgui_menusep(file);
+
 	testpanel(dtsgui, file);
+	tree = dtsgui_treewindow(dtsgui, "Tree Window");
+	dtsgui_newmenuitem(file, dtsgui, "T&ree", tree);
 
 	dtsgui_menusep(file);
 	dtsgui_close(file, dtsgui);
@@ -111,7 +314,7 @@ void help_menu(struct dtsgui *dtsgui) {
 
 	help = dtsgui_newmenu(dtsgui, "&Help");
 
-	p = dtsgui_textpane(dtsgui, "TEST", "Hello");
+	p = dtsgui_textpane(dtsgui, "TEST", "Hello This is a text box");
 	dtsgui_newmenuitem(help, dtsgui, "&Hello...\tCtrl-H", p);
 	dtsgui_menusep(help);
 	dtsgui_about(help, dtsgui, "This is a test application!!!!");
@@ -122,11 +325,14 @@ int guiconfig_cb(struct dtsgui *dtsgui, void *data) {
 
 	if (!data || !objref(appdata)) {
 		return 0;
+
 	}
 
 	/* menus*/
 	file_menu(dtsgui);
 	help_menu(dtsgui);
+
+	return 1;
 
 	/*load xml config via http*/
 	if (!(appdata->xmldoc = loadxmlconf(dtsgui))) {
@@ -134,14 +340,17 @@ int guiconfig_cb(struct dtsgui *dtsgui, void *data) {
 		dtsgui_confirm(dtsgui, "Config DL Failed (as expected after 3 tries)\nHello Dave\n\nWould You Like To Play .... Thermo Nuclear War ?");
 		return 1;
 	}
+
 	return 1;
 }
 
 void free_appdata(void *data) {
 	struct app_data *appdata = data;
+
 	if (appdata->dtsgui) {
 		objunref(appdata->dtsgui);
 	}
+
 	if (appdata->xmldoc) {
 		objunref(appdata->xmldoc);
 	}
@@ -157,9 +366,17 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	appdata->dtsgui = dtsgui_config(guiconfig_cb, appdata, wsize, wpos, "Wx Windows Test", "Welcome to wxWidgets!");
+	startthreads();
+	xml_init();
+	xslt_init();
+
+	appdata->dtsgui = dtsgui_config(guiconfig_cb, appdata, wsize, wpos, "Distrotech System App", "Welcome to Distrotech App!");
 	res = dtsgui_run(argc, argv);
 	objunref(appdata);
+
+	xslt_close();
+	xml_close();
+	stopthreads();
 
 	return res;
 }
