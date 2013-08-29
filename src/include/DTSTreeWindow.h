6551bc39a90c46dc497e3b2d9f09cd5d099e9e5c
Gregory Hinton Nietsky <gregory@distrotech.co.za>
2013-08-29 08:14:33 +0200

    Reformat

--- src/include/DTSTreeWindow.h
+++ src/include/DTSTreeWindow.h
@@ -2,18 +2,18 @@
 #define DTSTREEWINDOW_H
 
 class DTSTreeWindow: public DTSPanel, public virtual wxSplitterWindow {
-public:
-	DTSTreeWindow(wxWindow *parent = NULL, wxFrame *frame = NULL, wxString stat_msg = wxEmptyString, int pos = 30);
-	~DTSTreeWindow();
-	bool Show(bool = true);
-protected:
-	void SetWindow(wxWindow *window);
-private:
-	wxPanel *t_pane;
-	wxWindow *c_pane;
-	wxDataViewTreeCtrl *tree;
-	wxBoxSizer *sizer;
-	wxWindow *a_window;
+	public:
+		DTSTreeWindow(wxWindow *parent = NULL, wxFrame *frame = NULL, wxString stat_msg = wxEmptyString, int pos = 30);
+		~DTSTreeWindow();
+		bool Show(bool = true);
+	protected:
+		void SetWindow(wxWindow *window);
+	private:
+		wxPanel *t_pane;
+		wxWindow *c_pane;
+		wxDataViewTreeCtrl *tree;
+		wxBoxSizer *sizer;
+		wxWindow *a_window;
 };
 
 #endif // DTSTREEWINDOW_H
