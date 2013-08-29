c93efd78a680f63d0791c638bd5a9a5ba894cb3a
Gregory Hinton Nietsky <gregory@distrotech.co.za>
2013-08-29 08:14:05 +0200

    Add Support for xml based widgets add wizardpage panel

--- src/include/DTSPanel.h
+++ src/include/DTSPanel.h
@@ -21,11 +21,36 @@
 
 typedef class DTSPanel DTSPanel;
 
+struct xml_element {
+	struct xml_search *xsearch;
+	const char  *attr;
+};
+
+union form_data {
+	void *ptr;
+	struct xml_element *xml;
+};
+
+enum form_data_type {
+	DTSGUI_FORM_DATA_PTR,
+	DTSGUI_FORM_DATA_XML
+};
+
+struct form_item {
+	union form_data data;
+	enum form_data_type dtype;
+	enum widget_type type;
+	const char *name;
+	void *widget;
+};
+
 class DTSPanelEvent: public wxEvtHandler {
 	public:
 		DTSPanelEvent(void *userdata = NULL, event_callback evcb = NULL, DTSPanel *win = NULL);
-		void BindButton(wxPanel *win, int button);
+		void BindButton(wxWindow *win, int button);
+		void BindCombo(wxWindow *win, int button);
 		void OnButton(wxCommandEvent &event);
+		void OnCombo(wxCommandEvent &event);
 	private:
 		void *data;
 		event_callback evcb;
@@ -36,18 +61,26 @@ class DTSPanel: public virtual wxWindow {
 	public:
 		DTSPanel(wxFrame* = NULL, wxString = wxEmptyString, int = 0);
 		~DTSPanel();
-		wxTextCtrl *TextBox(const char *, wxString = wxEmptyString, int flags = wxTE_LEFT, int rows = 1);
-		wxTextCtrl *PasswdBox(const char *, wxString = wxEmptyString, int flags = wxTE_LEFT);
-		wxCheckBox *CheckBox(const char *title, int ischecked);
-		wxChoice *ListBox(const char *title);
-		void AddItem(wxWindow *item, const wxGBPosition pos, const wxGBSpan span = wxDefaultSpan, int flags = 0,int growrow = -1);
+		void Title(const char *title);
+		void TextBox(const char *, wxString = wxEmptyString, int flags = wxTE_LEFT, int rows = 1, void *data = NULL, enum form_data_type dtype = DTSGUI_FORM_DATA_PTR);
+		void PasswdBox(const char *, wxString = wxEmptyString, int flags = wxTE_LEFT, void *data = NULL, enum form_data_type dtype = DTSGUI_FORM_DATA_PTR);
+		void CheckBox(const char *title, int ischecked, void *data = NULL, enum form_data_type dtype = DTSGUI_FORM_DATA_PTR);
+		struct form_item *ListBox(const char *title, void *data = NULL, enum form_data_type dtype = DTSGUI_FORM_DATA_PTR);
+		struct form_item *ComboBox(const char *title, void *data = NULL, enum form_data_type dtype = DTSGUI_FORM_DATA_PTR);
+		void AddItem(wxWindow *item, const wxGBPosition pos, const wxGBSpan span = wxDefaultSpan, int flags = 0, int border = 0,	int growrow = -1);
 		void SetEventCallback(event_callback evcb, void *userdata = NULL);
 		void EventHandler(int eid, wxCommandEvent *event);
+		void SetUserData(void *data);
+		void *GetUserData(void);
+		struct bucket_list *GetItems(void);
+		void SetXMLDoc(struct xml_doc *xmldoc);
+		struct xml_element *GetNode(const char *xpath, const char *attr);
 		wxString GetName();
-		wxPanel *GetPanel();
+		wxWindow *GetPanel();
 		panel_type type;
 		int buttons[6];
 	protected:
+		void SetSizerSize(wxSize, wxWindow*);
 		bool ShowPanel(bool = true);
 		void SetupWin();
 		DTSPanelEvent *dtsevthandler;
@@ -55,11 +88,15 @@ class DTSPanel: public virtual wxWindow {
 		wxString status;
 		wxFrame *frame;
 		void Buttons(void);
-		wxPanel *panel;
+		wxWindow *panel;
 	private:
+		struct form_item *create_new_fitem(void *widget, enum widget_type type, const char *name, const char *value, void *data = NULL, enum form_data_type dtype = DTSGUI_FORM_DATA_PTR);
 		wxGridBagSizer *fgs;
 		bool beenshown;
 		int g_row;
+		void *userdata;
+		struct xml_doc  *xmldoc;
+		struct bucket_list *fitems;
 };
 
 class DTSStaticPanel: public DTSPanel, public virtual wxPanel  {
@@ -84,10 +121,17 @@ class DTSWindow: public DTSPanel, public virtual wxWindow {
 class DTSDialog: public DTSStaticPanel {
 	public:
 		DTSDialog(wxFrame* = NULL, wxString = wxEmptyString, int = wx_PANEL_BUTTON_ACTION);
-		virtual bool Show(bool = true);
+		bool Show(bool = true);
+		void RunDialog(void);
 	private:
 		wxBoxSizer *sizer;
 		wxDialog *dialog;
+};
+
+class DTSWizardWindow: public DTSPanel, public virtual wxWizardPageSimple {
+	public:
+		DTSWizardWindow(wxString = wxEmptyString);
+		virtual bool Show(bool = true);
 };
 
 #endif // DTSPANEL_H
